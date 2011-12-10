8f0bd4c4220644203bfa84098e3d379d55029641
juandebravo <juandebravo@gmail.com>
2011-12-11 01:54:58 +0200

    MINOR example using symbols instead of string to define channel type

--- bin/main.rb
+++ bin/main.rb
@@ -4,12 +4,12 @@ require 'connfu_experiment'
 
 ConnfuExperiment.start ARGV[0] do
   
-  listen "sms" do |msj|
-    puts msj.content
+  listen :sms do |msg|
+    puts "Just got an SMS from #{msg[:from]} to #{msg[:to]}: #{msg[:content]}"
   end
 
-  listen "rss" do |rss|
-    puts rss.content
+  listen :rss do |rss|
+    puts "Just got an RSS: #{rss[:content]}"
   end
 
 end
